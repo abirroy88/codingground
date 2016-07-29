cbf30c09907e19b40e5445e8ee5c8b8287782ba7
Abir Roy <abirkumarroy88@gmail.com>
2016-07-29 12:44:54 +0600

    Update main.c

--- New Project-20160729/main.c
+++ New Project-20160729/main.c
@@ -21,27 +21,3 @@ void main()
    }
    printf("\n");
 }
-
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int f(int arr[],int size){
-   int i=0;
-   for(i=0;i<size;i++){
-       arr[i] += 10;
-   }
-   return arr[3];
-}
-
-
-void main()
-{
-   int array[3] = {1,2,3};
-   int i;
-   f(array,3);
-   for(i=0;i<3;i++){
-       printf("%d,",array[i]);
-   }
-}
-
