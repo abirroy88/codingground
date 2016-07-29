1129afb490fcde7cc8c6933f51aca40421e8c73f
Abir Roy <abirkumarroy88@gmail.com>
2016-07-29 12:40:44 +0600

    Update main.c

--- New Project-20160729/main.c
+++ New Project-20160729/main.c
@@ -1,3 +1,28 @@
+// Taking and returning an int array - static
+#include <stdio.h>
+#include <stdlib.h>
+
+int f(int arr[],int size){
+   int i=0;
+   for(i=0;i<size;i++){
+       arr[i] += 10;
+   }
+   return arr[3];
+}
+
+
+void main()
+{
+   int array[3] = {1,2,3};
+   int i;
+   f(array,3);
+   for(i=0;i<3;i++){
+       printf("%d,",array[i]);
+   }
+   printf("\n");
+}
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
